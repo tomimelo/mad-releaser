a5431a795a851983212e09e23fa41c8cb8334a09
Tomas Melone <tomas.melone@es.stroeer-labs.com>
2022-08-25 13:34:47 +0200

    Project setup

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true
+  },
+  extends: 'standard-with-typescript',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    'no-unused-vars': 'off',
+    'no-useless-constructor': 0,
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'error',
+    '@typescript-eslint/explicit-member-accessibility': 'error',
+    '@typescript-eslint/member-ordering': 'error'
+  }
+}
