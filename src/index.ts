e81fbc127dcf16bc507b83165dde8edf26965a5b
Tomas Melone <tomas.melone@es.stroeer-labs.com>
2022-08-25 14:20:08 +0200

    [wip] test and first steps

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import shelljs from 'shelljs'
+
+const { echo } = shelljs
+
+echo('Hello from shell')
+
+//Check if git exist
+//Check if existent repository
+//Check branch name
+//Ask for type of release: Major, minor, patch
+//Checkout master (see main branch in config)
+//Merge
+//Run tests,
+//Run lint,
+//Push
